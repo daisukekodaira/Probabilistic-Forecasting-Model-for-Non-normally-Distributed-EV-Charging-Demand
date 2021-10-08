b7f3a7d0656e284069880149a3ed3a1fc935aceb
KeitaroOkada <7318039@ed.tus.ac.jp>
2021-10-08 14:27:19 +0900

    Update LSTMEV_Training.m

--- setEVModel/LSTMEV_Training.m
+++ setEVModel/LSTMEV_Training.m
@@ -1,13 +1,27 @@
 function LSTMEV_Training(trainData, colPredictors, path)
-data = LSTMEV_Training(trainData, colPredictors, path);
-data = [data{:}];
-mu = mean(data);
-sig = std(data);
 
-dataTrainStandardized = (dada - mu) / sig;
-XTrain = dataTrainStandardized(1:end-1);
-YTrain = dataTrainStandardized(2:end);
-numFeatures = 1;
+   %% Train the model for Energy Transition
+    % Training for Energy Trantision
+    % Training for SOC
+    trainedLSTM_EnergyTrans = LSTM_train(trainData, colPredictors, {'ChargeDischargeKwh'});
+    trainedLSTM_SOC = LSTM_train(trainData, colPredictors, 'SOCPercent');
+    
+    %% save result mat file
+    building_num = num2str(trainData.BuildingIndex(1));
+    save_name1 = '\EV_trainedLSTM_';
+    save_fullPath = strcat(path,save_name1,building_num,'.mat');
+    clearvars path;
+    save(save_fullPath, 'trainedLSTM_EnergyTrans', 'trainedLSTM_SOC');
+    
+end
+    
+function trainedLSTM = LSTM_train(trainData, columnPredictors, columnTarget)    
+
+n_instance = size(trainData,1);        
+x = transpose(table2array(trainData(1:n_instance, columnPredictors))); % input(feature)
+t = transpose(table2array(trainData(1:n_instance, columnTarget))); % target
+
+numFeatures = 8;
 numResponses = 1;
 numHiddenUnits = 200;
 
@@ -27,16 +41,12 @@ options = trainingOptions('adam', ...
     'Verbose',0, ...
     'Plots','training-progress');
 
-net = trainNetwork(XTrain,YTrain,layers,options);
+net = trainNetwork(x,t,layers,options);
+trainedLSTM = net; 
 
 
 
-%% save result mat file
-    building_num = num2str(trainData.BuildingIndex(1));
-    save_name1 = '\EV_trainedLSTM_';
-    save_fullPath = strcat(path,save_name1,building_num,'.mat');
-    clearvars path;
-    save(save_fullPath, 'LSTM_EnergyTrans', 'trainedNet_SOC');
+
 
 end
 
